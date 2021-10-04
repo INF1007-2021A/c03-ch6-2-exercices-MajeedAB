07b91248e5ab03a8fedea8bb8c057aa7c1639d2b
MajeedAB <majeed.abdul-baki@polymtl.ca>
2021-10-04 14:36:09 -0400

    fini UNE LIGNE PARTOUT

--- exercice.py
+++ exercice.py
@@ -6,26 +6,45 @@ from matplotlib.colors import cnames
 
 def list_to_dict(some_list: list) -> dict:
     # TODO: Transformer la liste en dictionnaire, les éléments de la liste deviennent les clés et leur index deviennent les valeurs
-
-    return {}
+    
+    # some_dict = {}
+    # for i,cle in enumerate(some_list):
+    #     some_dict[cle] = i
+    # return some_dict
+    return {cle:i for i,cle in enumerate(some_list)}
 
 
 def color_name_to_hex(colors: list) -> list:
     # TODO: Trouver la valeur hex de chaque couleur dans la liste et créer une liste de tupple où le premier élément est le nom de la couleur et le deuxième est la valeur hex
-
-    return []
+    # couleurs = [(coul,cnames[coul]) for coul in colors]
+    return [(col,cnames[col]) for col in colors]
 
 
 def create_list() -> list:
     # TODO: Créer une liste des 10 000 premiers entiers positif, sauf pour les entiers de 15 à 350
 
-    return []
+    return [i for i in range(10000)[0:15]]+[i for i in range(10000)[351:10000]]
 
 
 def compute_mse(model_dict: dict) -> dict:
     # TODO: Calculer l'erreur quadratique moyen pour chaque modèle. Retourner un dictionnaire contenant les MSE.
+    mse = {}
+    for model in model_dict:
+        erreur = 0
+        for point in model_dict[model]:
+            erreur+=(point[0]-point[1])**2
+        mse[model] = erreur/len(model_dict[model])
+    # return mse
 
-    return {}
+    # return {model:sum(p for )/len(model_dict[model]) for point in model_dict[model] for model in model_dict}
+
+
+    # return {model:sum((point[0]-point[1])**2 for point in model_dict[model])/len(model_dict[model]) for model in model_dict}
+
+    return{m:sum((p[0]-p[1])**2for p in model_dict[m])/len(model_dict[m])for m in model_dict}
+
+    # return{(lambda d:model_dict) (m:sum((p[0]-p[1])**2for p in d[m])/len(d[m])for m in d)}
+
 
 
 def main() -> None:
